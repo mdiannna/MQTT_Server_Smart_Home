9b25fc8fa6ecc87e56b383467595dc6fd74411d4
Diana <mdiannna@yahoo.com>
2020-06-28 13:57:36 +0300

    changed host to be accesible on 0.0.0.0

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ if __name__ =='__main__':
 	#app.run(host='0.0.0.0')
 	# manager.run()
 	# manager.run(debug=True, use_reloader=False)
-	app.run(debug=True, use_reloader=False)
+	app.run(debug=True, use_reloader=False, host='0.0.0.0')
 	# socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=True)
 
 
